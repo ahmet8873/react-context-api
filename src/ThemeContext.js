07fcdcc8170cec053f813ebc2e2df53ca37a1b66
Ahmet Avci <ahmetavci.8873@gmail.com>
2022-09-16 21:13:44 +0200

    use-context

---
+++ src/ThemeContext.js
@@ -0,0 +1,34 @@
+import React,{ useState, useContext, createContext } from "react"
+
+
+    const ThemeContext= React.createContext()
+    const ThemeUpdateContext=React.createContext()
+
+     export const useTheme=()=>{
+        return useContext(ThemeContext)
+    }
+
+   export  const useThemeUpdate=()=>{
+        return useContext(ThemeUpdateContext)
+    }
+
+
+  const ThemeProvider=({children})=>{
+        const [darkTheme, setDarkTheme]=useState(true)
+  
+ 
+        const toggleTheme=()=>{
+        setDarkTheme(prevDarkTheme=>!prevDarkTheme)
+    }
+    return (
+        <ThemeContext.Provider value={darkTheme}>
+            <ThemeUpdateContext.Provider value={toggleTheme}>
+                {children} 
+            </ThemeUpdateContext.Provider>
+           
+        </ThemeContext.Provider>
+    )
+
+  }
+
+  export default ThemeProvider
